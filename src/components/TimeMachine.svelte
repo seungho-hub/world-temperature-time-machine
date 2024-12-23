85804b28f68417943d03de65cc20fe1d473b3508
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2024-12-23 22:47:06 +0900

    real done

--- src/components/TimeMachine.svelte
+++ src/components/TimeMachine.svelte
@@ -43,11 +43,14 @@
 
 	let prediction = $state(0);
 
+	let loading = $state(false);
 	function fetchPrediction() {
+		loading = true;
 		fetch(`/api/predict?cc=${cc}&year=${year}`)
 			.then((res) => res.json())
 			.then((data) => {
 				prediction = parseFloat(data.prediction).toFixed(2);
+				loading = false;
 			});
 	}
 </script>
@@ -74,7 +77,9 @@
 			<span>년에는 표면 온도가 어느정도 증가할까요?</span>
 		</div>
 		<div class="prediction">
-			{#if prediction}
+			{#if loading}
+				<span class="loader"></span>
+			{:else if prediction}
 				<p>{prediction} °C 상승할 예정입니다</p>
 				<div class="description">
 					<ul>
@@ -172,7 +177,11 @@
 			.prediction {
 				display: flex;
 				flex-direction: column;
+				align-items: center;
+				justify-content: center;
 				gap: 20px;
+				height: auto;
+				min-height: 200px;
 				.description {
 					padding: 20px;
 					img {
@@ -187,6 +196,108 @@
 					}
 				}
 			}
+		}
+	}
+
+	.loader {
+		font-size: 10px;
+		width: 1em;
+		height: 1em;
+		border-radius: 50%;
+		position: relative;
+		text-indent: -9999em;
+		animation: mulShdSpin 1.1s infinite ease;
+		transform: translateZ(0);
+	}
+	@keyframes mulShdSpin {
+		0%,
+		100% {
+			box-shadow:
+				0em -2.6em 0em 0em #ffffff,
+				1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2),
+				2.5em 0em 0 0em rgba(255, 255, 255, 0.2),
+				1.75em 1.75em 0 0em rgba(255, 255, 255, 0.2),
+				0em 2.5em 0 0em rgba(255, 255, 255, 0.2),
+				-1.8em 1.8em 0 0em rgba(255, 255, 255, 0.2),
+				-2.6em 0em 0 0em rgba(255, 255, 255, 0.5),
+				-1.8em -1.8em 0 0em rgba(255, 255, 255, 0.7);
+		}
+		12.5% {
+			box-shadow:
+				0em -2.6em 0em 0em rgba(255, 255, 255, 0.7),
+				1.8em -1.8em 0 0em #ffffff,
+				2.5em 0em 0 0em rgba(255, 255, 255, 0.2),
+				1.75em 1.75em 0 0em rgba(255, 255, 255, 0.2),
+				0em 2.5em 0 0em rgba(255, 255, 255, 0.2),
+				-1.8em 1.8em 0 0em rgba(255, 255, 255, 0.2),
+				-2.6em 0em 0 0em rgba(255, 255, 255, 0.2),
+				-1.8em -1.8em 0 0em rgba(255, 255, 255, 0.5);
+		}
+		25% {
+			box-shadow:
+				0em -2.6em 0em 0em rgba(255, 255, 255, 0.5),
+				1.8em -1.8em 0 0em rgba(255, 255, 255, 0.7),
+				2.5em 0em 0 0em #ffffff,
+				1.75em 1.75em 0 0em rgba(255, 255, 255, 0.2),
+				0em 2.5em 0 0em rgba(255, 255, 255, 0.2),
+				-1.8em 1.8em 0 0em rgba(255, 255, 255, 0.2),
+				-2.6em 0em 0 0em rgba(255, 255, 255, 0.2),
+				-1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2);
+		}
+		37.5% {
+			box-shadow:
+				0em -2.6em 0em 0em rgba(255, 255, 255, 0.2),
+				1.8em -1.8em 0 0em rgba(255, 255, 255, 0.5),
+				2.5em 0em 0 0em rgba(255, 255, 255, 0.7),
+				1.75em 1.75em 0 0em #ffffff,
+				0em 2.5em 0 0em rgba(255, 255, 255, 0.2),
+				-1.8em 1.8em 0 0em rgba(255, 255, 255, 0.2),
+				-2.6em 0em 0 0em rgba(255, 255, 255, 0.2),
+				-1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2);
+		}
+		50% {
+			box-shadow:
+				0em -2.6em 0em 0em rgba(255, 255, 255, 0.2),
+				1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2),
+				2.5em 0em 0 0em rgba(255, 255, 255, 0.5),
+				1.75em 1.75em 0 0em rgba(255, 255, 255, 0.7),
+				0em 2.5em 0 0em #ffffff,
+				-1.8em 1.8em 0 0em rgba(255, 255, 255, 0.2),
+				-2.6em 0em 0 0em rgba(255, 255, 255, 0.2),
+				-1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2);
+		}
+		62.5% {
+			box-shadow:
+				0em -2.6em 0em 0em rgba(255, 255, 255, 0.2),
+				1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2),
+				2.5em 0em 0 0em rgba(255, 255, 255, 0.2),
+				1.75em 1.75em 0 0em rgba(255, 255, 255, 0.5),
+				0em 2.5em 0 0em rgba(255, 255, 255, 0.7),
+				-1.8em 1.8em 0 0em #ffffff,
+				-2.6em 0em 0 0em rgba(255, 255, 255, 0.2),
+				-1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2);
+		}
+		75% {
+			box-shadow:
+				0em -2.6em 0em 0em rgba(255, 255, 255, 0.2),
+				1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2),
+				2.5em 0em 0 0em rgba(255, 255, 255, 0.2),
+				1.75em 1.75em 0 0em rgba(255, 255, 255, 0.2),
+				0em 2.5em 0 0em rgba(255, 255, 255, 0.5),
+				-1.8em 1.8em 0 0em rgba(255, 255, 255, 0.7),
+				-2.6em 0em 0 0em #ffffff,
+				-1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2);
+		}
+		87.5% {
+			box-shadow:
+				0em -2.6em 0em 0em rgba(255, 255, 255, 0.2),
+				1.8em -1.8em 0 0em rgba(255, 255, 255, 0.2),
+				2.5em 0em 0 0em rgba(255, 255, 255, 0.2),
+				1.75em 1.75em 0 0em rgba(255, 255, 255, 0.2),
+				0em 2.5em 0 0em rgba(255, 255, 255, 0.2),
+				-1.8em 1.8em 0 0em rgba(255, 255, 255, 0.5),
+				-2.6em 0em 0 0em rgba(255, 255, 255, 0.7),
+				-1.8em -1.8em 0 0em #ffffff;
 		}
 	}
 </style>
